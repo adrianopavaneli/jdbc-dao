33c18c8647aa5441555adf8b37257a256166066d
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-08-10 21:38:17 -0300

    Criacao classes DaoFactory e SellerDaoJBDC

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import java.util.List;
+
 import model.entities.Department;
 
 public interface DepartmentDao {
@@ -7,5 +9,5 @@ public interface DepartmentDao {
 	void update(Department obj);
 	void deleteById(Department obj);
 	Department findById(Integer id);
-	
+	List<Department> findAll();
 }
